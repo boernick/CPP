6b4280ecfe0c61a15d5cf243f822953d3fd12e0f
Nick Boer <nboer@c3a4c9.42berlin.de>
2025-03-09 18:31:58 +0100

    update

--- cpp01/ex00/randomChump.cpp
+++ cpp01/ex00/randomChump.cpp
@@ -1,26 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/07 15:45:48 by nboer             #+#    #+#             */
-/*   Updated: 2025/03/09 18:31:40 by nboer            ###   ########.fr       */
+/*   Created: 2025/03/09 18:17:34 by nboer             #+#    #+#             */
+/*   Updated: 2025/03/09 18:17:49 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "Zombie.hpp"
+#include "Zombie.hpp"
 
-Zombie::Zombie(std::string _name){
-	name = _name;
-	std::cout << name << " spawned" << std::endl;	
-}
-
-Zombie::~Zombie(void){
-	std::cout << name << " died" << std::endl;
-}
-
-void	Zombie::announce(void){
-	std::cout << name << ": " << ZOMBIE_MSG << std::endl;
+void	randomChump(std::string name)
+{
+	Zombie z(name);
+	z.announce();
 }
