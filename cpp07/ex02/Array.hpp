e35fbe47a17d0cf642f063eeed873f48319c9d99
Nick Boer <nboer@c1b2c1.42berlin.de>
2025-08-31 15:39:09 +0200

    update

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,23 +6,67 @@
 /*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 18:17:54 by nboer             #+#    #+#             */
-/*   Updated: 2025/08/30 18:25:24 by nboer            ###   ########.fr       */
+/*   Updated: 2025/08/31 15:29:06 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_H
 #define ARRAY_H
 
+#include <exception>
+
 template<typename T>
 class Array {
 	private:
+		T				*_array;
+		unsigned int	_n;
 	public:
-		Array<T>();
-		Array(unsigned int n);
-		~Array();
-		Array(Array &other);
-		&operator=(Array &other);
-		const int size() const;
+		class OutsideBoundsException : public std::exception {
+			public:
+				virtual const char* what() const throw() {
+					return "Index out of bounds";
+			}
+		};
+		
+		Array<T>(){
+			_array = new T[0];
+			_n = 0;
+		}
+		
+		Array<T>(unsigned int n){
+			_array = new T[n]();
+			_n = n;
+		}
+		
+		~Array() {
+			delete[] _array;
+		}
+		
+		Array(Array &other) {
+			_array = new T[other._n];
+			_n = other._n;
+		}
+		
+		Array<T> &operator=(const Array<T> &other) {
+			if (this != &other){
+				delete[] _array;
+				_array = new T[other._n];
+				_n = other._n;
+				for (unsigned int i = 0; i < other._n; i++)
+					_array[i] = T(other._array[i]);
+			}
+			return *this;
+		}
+
+		T &operator[](unsigned int i) const {
+		if (i >= _n)
+			throw OutsideBoundsException();
+		return _array[i];
+	}
+		
+		unsigned int size() const {
+			return _n;
+		}
 };
 
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,12 +6,45 @@
 /*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 18:16:50 by nboer             #+#    #+#             */
-/*   Updated: 2025/08/30 18:17:47 by nboer            ###   ########.fr       */
+/*   Updated: 2025/08/31 15:38:59 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
+#include <iostream>
 
 int main(void) {
+	//integers
+	Array<int> empty_array;
+	std::cout << "Size array: " << empty_array.size() << std::endl;
+	
+	Array<int> array_5(5);
+	Array<int> array_3(3);
+	try {
+		array_3[4] = 0;
+		array_5[4] = 0;
+	}
+	catch (const Array<int>::OutsideBoundsException &e) {
+		std::cout << "Exception caught: " << e.what() << std::endl;
+	}
+	
+	array_3 = array_5;
+	
+	try {
+		array_5[4] = 1;
+	}
+	catch (const Array<int>::OutsideBoundsException &e) {
+		std::cout << "Exception caught: " << e.what() << std::endl;
+	}
+	for (unsigned int i = 0; i < array_3.size(); i++)
+		std::cout << array_3[i];
+	std::cout << std::endl;
+	for (unsigned int i = 0; i < array_5.size(); i++)
+		std::cout << array_5[i];
+	std::cout << std::endl;
+
+	//strings
+	........
+
 	
 }
