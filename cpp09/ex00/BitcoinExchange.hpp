d71f9baf09036e5ed6bdb0c8fbdd4e7f4a2e5660
Nick Boer <nboer@c3a3c6.42berlin.de>
2026-01-11 19:43:59 +0100

    start bitcoinexchange

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 19:17:49 by nboer             #+#    #+#             */
-/*   Updated: 2026/01/11 19:43:12 by nboer            ###   ########.fr       */
+/*   Updated: 2026/01/11 19:43:46 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,4 @@ class BitcoinExchange {
 		BitcoinExchange &operator=(const BitcoinExchange &src);
 	private:
 		std::map<time_t, double> _data;
-}
+};
